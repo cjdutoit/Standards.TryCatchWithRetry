849c29dc72515cf4803ba8ca34263397feee52e9
Christo du Toit <christo@dutoit.co.uk>
2022-08-16 16:09:03 +0100

    BROKERS: Storage - Select Student By Id

--- Standards.TryCatchWithRetry.Api/Brokers/Storages/IStorageBroker.Students.cs
+++ Standards.TryCatchWithRetry.Api/Brokers/Storages/IStorageBroker.Students.cs
@@ -9,5 +9,6 @@ namespace Standards.TryCatchWithRetry.Api.Brokers.Storages
     {
         ValueTask<Student> InsertStudentAsync(Student student);
         IQueryable<Student> SelectAllStudents();
+        ValueTask<Student> SelectStudentByIdAsync(Guid studentId);
     }
 }
