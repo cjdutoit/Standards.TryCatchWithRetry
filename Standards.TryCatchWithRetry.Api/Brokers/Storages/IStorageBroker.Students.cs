1a5b8a6ef0e0dd0c132e7660e3dd7539eca46743
Christo du Toit <christo@dutoit.co.uk>
2022-08-16 16:09:08 +0100

    BROKERS: Storage - Update Student

--- Standards.TryCatchWithRetry.Api/Brokers/Storages/IStorageBroker.Students.cs
+++ Standards.TryCatchWithRetry.Api/Brokers/Storages/IStorageBroker.Students.cs
@@ -10,5 +10,6 @@ namespace Standards.TryCatchWithRetry.Api.Brokers.Storages
         ValueTask<Student> InsertStudentAsync(Student student);
         IQueryable<Student> SelectAllStudents();
         ValueTask<Student> SelectStudentByIdAsync(Guid studentId);
+        ValueTask<Student> UpdateStudentAsync(Student student);
     }
 }
