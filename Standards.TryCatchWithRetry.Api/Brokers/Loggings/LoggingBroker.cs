1449b40e35394bc5d62af5d68b0e526ab7271aa8
Christo du Toit <christo@dutoit.co.uk>
2022-08-16 16:07:01 +0100

    BROKERS: Logging - Log LogTrace

--- Standards.TryCatchWithRetry.Api/Brokers/Loggings/LoggingBroker.cs
+++ Standards.TryCatchWithRetry.Api/Brokers/Loggings/LoggingBroker.cs
@@ -12,5 +12,8 @@ namespace Standards.TryCatchWithRetry.Api.Brokers.Loggings
 
         public void LogInformation(string message) =>
             this.logger.LogInformation(message);
+
+        public void LogTrace(string message) =>
+            this.logger.LogTrace(message);
     }
 }
