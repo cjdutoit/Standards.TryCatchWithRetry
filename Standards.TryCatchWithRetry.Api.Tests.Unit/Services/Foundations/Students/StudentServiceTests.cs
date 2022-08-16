29d77ae00c59b26e34bee0eee70e69a04d7301be
Christo du Toit <christo@dutoit.co.uk>
2022-08-16 16:28:28 +0100

    ShouldThrowValidationExceptionOnAddIfCreatedDateIsNotRecentAndLogItAsync -> FAIL

--- Standards.TryCatchWithRetry.Api.Tests.Unit/Services/Foundations/Students/StudentServiceTests.cs
+++ Standards.TryCatchWithRetry.Api.Tests.Unit/Services/Foundations/Students/StudentServiceTests.cs
@@ -8,6 +8,7 @@ using Standards.TryCatchWithRetry.Api.Models.Students;
 using Standards.TryCatchWithRetry.Api.Services.Foundations.Students;
 using Tynamix.ObjectFiller;
 using Xeptions;
+using Xunit;
 
 namespace Standards.TryCatchWithRetry.Api.Tests.Unit.Services.Foundations.Students
 {
@@ -33,8 +34,23 @@ namespace Standards.TryCatchWithRetry.Api.Tests.Unit.Services.Foundations.Studen
         private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
             actualException => actualException.SameExceptionAs(expectedException);
 
+        public static TheoryData MinutesBeforeOrAfter()
+        {
+            int randomNumber = GetRandomNumber();
+            int randomNegativeNumber = GetRandomNegativeNumber();
+
+            return new TheoryData<int>
+            {
+                randomNumber,
+                randomNegativeNumber
+            };
+        }
+
         private static int GetRandomNumber() =>
             new IntRange(min: 2, max: 10).GetValue();
+
+        private static int GetRandomNegativeNumber() =>
+            -1 * new IntRange(min: 2, max: 10).GetValue();
 
         private static DateTimeOffset GetRandomDateTimeOffset() =>
             new DateTimeRange(earliestDate: new DateTime()).GetValue();
